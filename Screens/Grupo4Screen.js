4d61994690f4d5f4b69b324c47df856849b07f30
wlady3190 <wlady_3190@hotmail.com>
2023-07-27 08:02:45 -0500

    adding group 2 - modified

--- Screens/Grupo4Screen.js
+++ Screens/Grupo4Screen.js
@@ -4,10 +4,7 @@ import React from 'react'
 export default function Grupo4Screen() {
   return (
     <View>
-      <Text>INTEGRANTES DEL GRUPO</Text>
-      <Text>Christian Hidalgo</Text>
-      <Text>WLadimir Tierra</Text>
-      <Text>Jorge Urgilés</Text>
+      <Text>Grupo4Screen</Text>
     </View>
   )
 }
