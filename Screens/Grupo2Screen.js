4ce4955eaa8bb8ff0deb6900b6170d606709c936
jroserog <jroserog@itsqmet.edu.ec>
2023-07-27 08:24:41 -0500

    Estilos

--- Screens/Grupo2Screen.js
+++ Screens/Grupo2Screen.js
@@ -1,14 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function Grupo2Screen() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>GRUPO 2</Text>
       <Text>Christian Hidalgo</Text>
       <Text>Wladimir Tierra</Text>
       <Text>Jorge Urgilés</Text>
+      <Text>Juan Paz</Text>
       
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f2ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
