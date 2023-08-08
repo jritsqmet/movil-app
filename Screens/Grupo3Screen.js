54cf5cb3848b28f4be80b1d46628b82bac2f34dd
juliorosero1 <58198049+juliorosero1@users.noreply.github.com>
2023-08-07 22:39:22 -0500

    find

--- Screens/Grupo3Screen.js
+++ Screens/Grupo3Screen.js
@@ -6,87 +6,88 @@ import userJSON from '../assets/data/users.json'
 
 export default function Grupo3Screen() {
 
-    const [usuario, setusuario] = useState("");
-    const [contrasenia, setcontrasenia] = useState("");
+  const [usuario, setusuario] = useState("");
+  const [contrasenia, setcontrasenia] = useState("");
 
-    //const [listaUsuarios, setlistaUsuarios] = useState(userJSON.users)
-    const [listaUsuarios, setlistaUsuarios] = useState(null)
+  //const [listaUsuarios, setlistaUsuarios] = useState(userJSON.users)
+  const [listaUsuarios, setlistaUsuarios] = useState(null)
 
-    // USE EFFECT
-    const API_USER= "https://my-json-server.typicode.com/jritsqmet/jsonpass/db"
-   
-   /*
-    useEffect(() => {
+  // USE EFFECT
+  const API_USER = "https://my-json-server.typicode.com/jritsqmet/jsonpass/db"
+
+  /*
+   useEffect(() => {
+     fetch(API_USER)
+     .then( response => response.json())
+     .then( datos => setlistaUsuarios(datos))
+   }, []);
+   */
+
+  useEffect(() => {
+    if (listaUsuarios == null) {
       fetch(API_USER)
-      .then( response => response.json())
-      .then( datos => setlistaUsuarios(datos))
-    }, []);
-    */
-    
-     useEffect(() => {
-      if( listaUsuarios == null){
-        fetch(API_USER)
-        .then( response => response.json())
-        .then( datos => setlistaUsuarios(datos))
-      }
-    }, [listaUsuarios])
-    
-    console.log(listaUsuarios.users)
-
-
-    function verificarUsuario( user, pass){
-     /*
-      for (let item of listaUsuarios.users){
-          if( user  == item.username){
-            if( pass == item.password){
-              Alert.alert( "MENSAJE", 'Acceso correcto' )
-            }else{
-              Alert.alert("MENSAJE", "Credenciales incorrectas")
-            }
-          }
-      }
-      */
-
-      //const found = array1.find((element) => element > 10);
-  
-      listaUsuarios.users.find( (user) => user >=0 )
-
-      if( listaUsuarios.users.find( (user) => user >=0 ) ){
-        if( pass == user[user].password ){
-          Alert.alert( "MENSAJE", 'Acceso correcto' )
-        }else{
-          Alert.alert("MENSAJE", "Credenciales incorrectas")
-        }
-      }else{
-        console.log("error")
-      }
-
-
+        .then(response => response.json())
+        .then(datos => setlistaUsuarios(datos.users))
     }
-  
+    console.log(listaUsuarios)
+  }, [listaUsuarios])
+
+  //console.log(listaUsuarios.users)
 
 
-  return (
-
-    <View style= {styles.container}>
-      <Text style={{fontSize: 35}}>EJERCICIO 3</Text>
-      <TextInput 
-        placeholder='Ingrese usuario'
-        style={styles.input}
-        onChangeText={ ( texto ) => setusuario( texto ) } 
-      />
-
-      <TextInput 
-        placeholder='Ingrese contraseña'
-        style={styles.input}
-        onChangeText={ ( texto ) => setcontrasenia( texto ) }
-      />
-
-      <Button title='LogIn' onPress={()=> verificarUsuario( usuario, contrasenia  )}/>
+  function verificarUsuario(user, pass) {
+    if (listaUsuarios.find( item => (item.username === user) && (item.password === pass))) {
+        Alert.alert("SIIII")
+ 
+    } else {
+      console.log("error")
+    }
+  }
 
 
-    </View>
-  )
+  /*
+   for (let item of listaUsuarios.users){
+       if( user  == item.username){
+         if( pass == item.password){
+           Alert.alert( "MENSAJE", 'Acceso correcto' )
+         }else{
+           Alert.alert("MENSAJE", "Credenciales incorrectas")
+         }
+       }
+   }
+   */
+
+  //const found = array1.find((element) => element > 10);
+
+  // listaUsuarios.users.find( (user) => user >=0 )
+
+
+
+
+
+
+
+return (
+
+  <View style={styles.container}>
+    <Text style={{ fontSize: 35 }}>EJERCICIO 3</Text>
+    <TextInput
+      placeholder='Ingrese usuario'
+      style={styles.input}
+      onChangeText={(texto) => setusuario(texto)}
+    />
+
+    <TextInput
+      placeholder='Ingrese contraseña'
+      style={styles.input}
+      onChangeText={(texto) => setcontrasenia(texto)}
+    />
+
+    <Button title='LogIn' onPress={() => verificarUsuario(usuario, contrasenia)} />
+
+
+  </View>
+)
 }
 
 const styles = StyleSheet.create({
